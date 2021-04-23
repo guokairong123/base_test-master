25c9efb2fb8a19aa6994c91240e3c07a47796fa0
guokairong <992926186@qq.com>
2021-04-23 15:25:25 +0800

    pytest

--- testing/conftest.py
+++ testing/conftest.py
@@ -17,3 +17,9 @@ def conn_db():
     print("数据库链接")
     yield "database"
     print("关闭数据库链接")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
--- testing/test_fixture.py
+++ testing/test_fixture.py
@@ -1,21 +1,29 @@
+from time import sleep
+
 import pytest
 
 
+@pytest.mark.run(order=3)
 @pytest.mark.usefixtures("login")
 def test_case1():
     print("用例1")
 
 
-def test_case2():
+@pytest.mark.run(order=4)
+@pytest.mark.parametrize('a', [1, 2, 3])
+def test_case2(a):
+    sleep(2)
     print("用例2")
 
 
+@pytest.mark.run(order=2)
 def test_case3(login, conn_db):
     print(login)
     print(conn_db)
     print("用例3")
 
 
+@pytest.mark.run(order=1)
 def test_case4():
     pytest.assume(True)
     pytest.assume(False)
